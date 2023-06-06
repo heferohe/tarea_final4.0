48563befcef525d98aa0adad4681f6decdc527ea
Hefero <134904707+heferohe@users.noreply.github.com>
2023-06-06 01:00:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ def home():
         # Aquí es donde procesarías el texto. Por ahora, solo devolvemos el mismo texto.
         source_language = ''
         indice = text1+text2+text3+text4+text5+text6+text7
-       # knn = joblib.load('modelo_entrenado.pkl') # Carga del modelo.
+        # knn = joblib.load('modelo_entrenado.pkl') # Carga del modelo.
        "# indice =knn.predict([[21.0,72.0,37.8,65.6,70.8,60.0,60.0]])
 
         return render_template('home.html', indice=indice,lang_detected=source_language)
